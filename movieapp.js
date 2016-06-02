c592a85bb4bfdc2fcebf57923fe6de8fe576f513
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-06-02 16:35:25 -0400

    added search feature

--- movieapp.js
+++ movieapp.js
@@ -22,18 +22,26 @@ app.controller('MainController', function($scope, $http) {
 
   });
   //when the more button is clicked show more movies
-    var page = 2;
+  var page = 2;
   $scope.getMoreMovies = function() {
-      $http.get('http://api.themoviedb.org/3/movie/now_playing?api_key=fec8b5ab27b292a68294261bb21b04a5&page=' + page)
-      .success(function(data) {
-        $scope.data = data;
-        //console.log(data);
-        //console.log(data.total_pages)
-        if(page <= data.total_pages) {
-          page++;
-        };
-      });
+    $http.get('http://api.themoviedb.org/3/movie/now_playing?api_key=fec8b5ab27b292a68294261bb21b04a5&page=' + page)
+    .success(function(data) {
+      $scope.data = data;
+      console.log(data);
+      //console.log(data.total_pages)
+      if(page <= data.total_pages) {
+        page++;
+      };
+    });
   };
+  //search for movies when search is clicked
+  $scope.searchMovies = function() {
+    $http.get("http://api.themoviedb.org/3/search/movie?api_key=fec8b5ab27b292a68294261bb21b04a5&query=" + $scope.searchMovie)
+    .success(function(data){
+      $scope.data = data;
+    });
+  };
+
 });
 
 app.controller('DetailsController', function($scope, $routeParams, $http) {
